7654e0601338868d17a5be3d28c092a1cb08449a
Omkar Upadhyay <omkarmh009@gmail.com>
2026-08-10 21:32:16 +0530

    First Commit

---
+++ src/test/java/com/ou_solutions/url_shortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.url_shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
