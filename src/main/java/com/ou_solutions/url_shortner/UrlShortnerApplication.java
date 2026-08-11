5f1ac8f3427a14afd726e25fb0d1ffbbd0cf5d17
Omkar Upadhyay <omkarmh009@gmail.com>
2026-08-11 20:03:28 +0530

    Enable JPA Auditing for auto date for create and modified

--- src/main/java/com/ou_solutions/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/ou_solutions/url_shortner/UrlShortnerApplication.java
@@ -2,8 +2,10 @@ package com.ou_solutions.url_shortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UrlShortnerApplication {
 
 	public static void main(String[] args) {
