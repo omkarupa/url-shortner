1aef6f7f991b13b1b1f7ae72b6f47a216f7187cc
Omkar Upadhyay <omkarmh009@gmail.com>
2026-08-11 20:02:57 +0530

    URL service with business logic

--- src/main/java/com/ou_solutions/url_shortner/service/URLService.java
+++ src/main/java/com/ou_solutions/url_shortner/service/URLService.java
@@ -1,17 +1,61 @@
 package com.ou_solutions.url_shortner.service;
 
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.ou_solutions.url_shortner.dto.URLDto;
+import com.ou_solutions.url_shortner.dto.URLShortner;
+import com.ou_solutions.url_shortner.model.URLShortnerDO;
+import com.ou_solutions.url_shortner.repository.URLRepo;
+import com.ou_solutions.url_shortner.util.Base62Encoder;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class URLService {
+	
+	private final URLRepo repo;
+	
+	private final ModelMapper mapper;
 
-	public URLDto createShortURL(String url) {
+	public URLDto createShortURL(URLShortner url) {
 		
-		return null;
+		Optional<URLShortnerDO> urlOptional = this.repo.findByOriginalURL(url.getOriginalURL());
+		
+		Optional<URLShortnerDO> urlAliasOptional = this.repo.findByShortURL(url.getShortURL());
+		
+		if(!urlOptional.isPresent())
+		{
+			if(url.getShortURL() != null && !url.getShortURL().isEmpty()  && !url.getShortURL().isBlank())
+			{
+				if(!urlAliasOptional.isPresent())
+				{
+					URLShortnerDO savedURLDO = this.repo.save( this.mapper.map(url, URLShortnerDO.class));
+					
+					return this.mapper.map(savedURLDO, URLDto.class);
+					
+				}
+				
+				throw new RuntimeException("Alias already exists");	
+			}
+			else {
+				URLShortnerDO savedURLDO = repo.save( mapper.map(url, URLShortnerDO.class));
+				
+				 savedURLDO.setShortURL(Base62Encoder.encode(savedURLDO.getId()));
+				 
+				 savedURLDO = this.repo.save(savedURLDO);
+				 
+				return mapper.map(savedURLDO, URLDto.class);
+			}
+			
+				
+		}
 		
 		
+		throw new RuntimeException("URL already has shortned URL");		
 	}
 
 	
